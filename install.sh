1c26836166c4c9bc0a97d17280b26a1d73e85623
pengrenliang <pengrenliang@91waijiao.com>
2014-07-23 17:46:53 +0800

    add install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@ if [ -d /usr/include/chef ]; then
 else
     mkdir -p /usr/include/chef
 fi
-if [ -d /usr/include/chef/chef_base ]; then
-    :
-else
-    mkdir -p /usr/include/chef/chef_base
-fi
-if [ -d /usr/include/chef/chef_io ]; then
-    :
-else
-    mkdir -p /usr/include/chef/chef_io
-fi
+#if [ -d /usr/include/chef/chef_base ]; then
+#    :
+#else
+#    mkdir -p /usr/include/chef/chef_base
+#fi
+#if [ -d /usr/include/chef/chef_io ]; then
+#    :
+#else
+#    mkdir -p /usr/include/chef/chef_io
+#fi
 
-cp chef_base/*.h /usr/include/chef/chef_base/
-cp chef_io/io_tcp.h /usr/include/chef/chef_io/
+cp chef_base/*.h /usr/include/chef/
+cp chef_io/io_tcp.h /usr/include/chef/
 cp lib/libchefbase.so lib/libchefio.so /usr/lib/
