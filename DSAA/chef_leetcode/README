2831fd7dd60563b27039584be82ca3cf22e8de2c
pengrenliang <pengrenliang@baijiahulian.com>
2014-12-27 14:06:29 +0800

    add two sum

---
+++ DSAA/chef_leetcode/README
@@ -0,0 +1,1 @@
+https://oj.leetcode.com/
--- DSAA/chef_sort/chef_buddle_sort.c
+++ DSAA/chef_sort/chef_buddle_sort.c
@@ -2,9 +2,9 @@
 
 void chef_buddle_sort(int arr[], int size) 
 {
-    int i,j,bak;
+    int i, j, bak;
     j = size - 1;
-    for (; j>0; --j) {
+    for (; j > 0; --j) {
         i = 0;
         for (; i < j; ++i) {
             if (arr[i] > arr[i + 1]) {
